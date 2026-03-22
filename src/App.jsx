c69aa93ade6f93c81a95753bde7dfcf0ec2856bc
JATIN KUMAR <jatinkumar638881@gmail.com>
2026-03-22 14:19:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind + Bootstrap Working 🚀
+      </h1>
+
+      <button className="btn btn-primary mt-3">
+        Bootstrap Button
+      </button>
+    </div>
+  );
+}
+export default App;
